35c63591f8bf9e15d004b013d8c360eaa655e0d1
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-08-22 15:14:07 +0700

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,45 +4,46 @@
         <div class="row">
 
             <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Bethany</h3>
-            <p>
-                A108 Adam Street <br>
-                New York, NY 535022<br>
-                United States <br><br>
-                <strong>Phone:</strong> +1 5589 55488 55<br>
-                <strong>Email:</strong> info@example.com<br>
-            </p>
+                <img src="{{asset('assets/img/HMTP.jpg')}}" style="max-height: 250px;max-width: 250px;vertical-align: middle;border: 0;">
+                {{-- <p>
+                    A108 Adam Street <br>
+                    New York, NY 535022<br>
+                    United States <br><br>
+                    <strong>Phone:</strong> +1 5589 55488 55<br>
+                    <strong>Email:</strong> info@example.com<br>
+                </p> --}}
             </div>
-
-            <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
+            <div class="col-lg-4 col-md-6 footer-newsletter">
+                <h4>Alamat : </h4>
+                <p>Jl. Kuaro, Gn. Kelua, Kec. Samarinda Ulu, Kabupaten Paser, Kalimantan Timur 75119</p>
+                {{-- <form action="" method="post">
+                    <input type="email" name="email"><input type="submit" value="Subscribe">
+                </form> --}}
             </div>
 
             <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
+                <h4>Kontak</h4>
+                <ul>
+                  <p>FB : hmtp unmul<br>
+                    Email :hmtp.unmul.smd@gmail.com
+                  </p>
+                </ul>
             </div>
 
-            <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Join Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-                <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
+            <div class="col-lg-2 col-md-6 footer-links">
+                <h4>Temukan Kami</h4>
+                <ul>
+                    <li><a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    </li>
+                    <li><a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                    </li>
+                </ul>
             </div>
+
+
 
         </div>
         </div>
